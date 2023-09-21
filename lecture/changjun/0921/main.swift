2268b54c17cc44391dcbe79f6e73ec7484fba571
Phangg <leeckdwns9305@gmail.com>
2023-09-21 16:57:34 +0900

    changjun-230921

---
+++ lecture/changjun/0921/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  prac2
+//
+//  Created by phang on 2023/09/21.
+//
+
+import Foundation
+
+//print("입력해주세요: ", terminator: "")
+//var input = readLine()!
+//print(input)
+
+let calculation = Calculation()
+calculation.exercise05()
+calculation.exercise06()
+calculation.exercise07()
+calculation.exercise08()
+calculation.exercise09()
+calculation.exercise10()
+
+let conditional = Conditional()
+conditional.exercise01()
+conditional.exercise02()
+conditional.exercise03()
+conditional.exercise04()
+conditional.exercise05()
+conditional.exercise06()
