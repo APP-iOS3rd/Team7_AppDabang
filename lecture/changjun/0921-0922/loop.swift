5dff42780943f7ecff3ea134d0d1051ec5908e36
Phangg <leeckdwns9305@gmail.com>
2023-09-22 18:21:52 +0900

    chanjun 0922 conditional + loop [edit]

--- lecture/changjun/0921-0922/loop.swift
+++ lecture/changjun/0921-0922/loop.swift
@@ -29,6 +29,16 @@ struct Loop {
         return input
     }
     
+    // MARK: - [ Y or N 답변 입력 함수 ]
+    private func getAnswerInputValue(message msg: String) -> String {
+        print("\(msg)", terminator: "")
+        guard let input = readLine(), (input == "N" || input == "Y") else {
+            print("입력 값 오류 - 올바른 값을 입력하세요.")
+            return getAnswerInputValue(message: msg)
+        }
+        return input
+    }
+    
     // MARK: - while 예제 1
     /*
      while문을 사용하여 키보드로부터 입력 받은 수가 10보다 작을 때만 계속 정수를 입력 받으세요.
@@ -95,6 +105,7 @@ struct Loop {
      비밀번호를 입력해 주세요: 1234
      로그인 성공!
      */
+    
     func whileExercise04() {
         let userID = "Hello"
         let password = 1234
@@ -114,4 +125,105 @@ struct Loop {
         }
     }
     
+    // MARK: - while 예제 5
+    /*
+     Login프로그램
+     아이디와 비밀번호가 틀렸을 경우 "계속 하시겠습니까?" 라는 문장을 출력하세요.
+     Y를 입력하면 아이디 비밀번호 입력 계속,
+     N을 입력하면 종료
+     로그인 성공 시 종료
+     
+     아이디를 입력해 주세요: hi
+     비밀번호를 입력해 주세요: 1234
+     아이디와 비밀번호가 잘못되었습니다.
+     계속 하시겠습니까? (Y/N) Y
+
+     아이디를 입력해 주세요: Hello
+     비밀번호를 입력해 주세요: 4321
+     아이디와 비밀번호가 잘못되었습니다.
+     계속 하시겠습니까? (Y/N) N
+
+     종료되었습니다.
+     아이디를 입력해 주세요: Hello
+     비밀번호를 입력해 주세요: 4321
+     아이디와 비밀번호가 잘못되었습니다.
+     계속 하시겠습니까? (Y/N) Y
+
+     아이디를 입력해 주세요: Hello
+     비밀번호를 입력해 주세요: 1234
+     로그인 성공!
+
+     종료되었습니다.
+     */
+    func whileExercise05() {
+        let userID = "Hello"
+        let password = 1234
+        
+        while true {
+            let inputID = getStringInputValue(message: "아이디를 입력해 주세요: ")
+            let inputPW = getIntTypeInputValue(message: "비밀번호를 입력해 주세요: ")
+            
+            if userID == inputID, password == inputPW {
+                print("로그인 성공!")
+                break
+            } else {
+                print("아이디와 비밀번호가 잘못되었습니다.")
+                let answer = getAnswerInputValue(message: "계속 하시겠습니까? (Y/N): ")
+                if answer == "N" {
+                    break
+                }
+            }
+        }
+        print("종료되었습니다.")
+    }
+    
+    // MARK: - while 예제 6
+    /*
+     (+, -) 계산기프로그램
+     첫 번째 정수를 입력 받는다.
+     두 번째 정수를 입력 받는다.
+     연산자를 선택한다.
+     선택한 연산자에 따라 연산결과를 출력한다.
+     다시 실행할 것인가를 물어본다.
+     “Y” 를 입력하면 다시 실행.
+     “N”을 입력하면 반복문 종료.
+     
+     첫 번째 정수를 입력하세요: 50
+     두 번째 정수를 입력하세요: 25
+     1.더하기 2.빼기 : 1
+     더하기 연산 결과는 75입니다.
+     다시 실행 하시겠습니까? (Y/N) Y
+
+     첫 번째 정수를 입력하세요: 30
+     두 번째 정수를 입력하세요: 46
+     1.더하기 2.빼기 : 2
+     빼기 연산 결과는 -16입니다.
+     다시 실행 하시겠습니까? (Y/N) N
+
+     종료되었습니다.
+     */
+    func whileExercise06() {
+        let num1 = getIntTypeInputValue(message: "첫 번째 정수를 입력하세요: ")
+        let num2 = getIntTypeInputValue(message: "두 번째 정수를 입력하세요: ")
+        let pickNumber = getIntTypeInputValue(message: "1.더하기 2.빼기 : ")
+        
+        while true {
+            if pickNumber == 1 {
+                print("더하기 연산 결과는 \(num1 + num2)입니다.")
+                break
+            } else if pickNumber == 2 {
+                print("빼기 연산 결과는 \(num1 - num2)입니다.")
+                break
+            }
+        }
+        
+        while true {
+            let answer = getAnswerInputValue(message: "다시 실행 하시겠습니까? (Y/N): ")
+            if answer == "N" {
+                break
+            }
+        }
+        print("종료되었습니다.")
+    }
+        
 }
