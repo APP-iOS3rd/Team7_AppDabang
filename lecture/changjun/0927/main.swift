1c307280926aab73e3f6402bb6214919f2561929
Phangg <leeckdwns9305@gmail.com>
2023-09-27 18:35:21 +0900

    :sparkles: TIL: class, Array
    excercise 코드

---
+++ lecture/changjun/0927/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  prac2
+//
+//  Created by phang on 2023/09/21.
+//
+
+import Foundation
+
+let myTV = TV(name: "LG", year: 2023, size: 48)
+//print(myTV.show())
+
+let arrayExercise01_05 = ArrayExercise01_05(arrCount: 10)
+//arrayExercise01_05.arrayExercise01()
+//print(arrayExercise01_05.arrayExercise02())
+//print(arrayExercise01_05.arrayExercise03())
+//print(arrayExercise01_05.arrayExercise04())
+//print(arrayExercise01_05.arrayExercise05())
+
+//let arrayExercise06 = ArrayExercise06(stringArr: ["수", "박"])
+
+//let arrayExercise07 = ArrayExercise07(arrCount: 10)
+
+let arrayExercise08_09 = ArrayExercise08_09()
+//arrayExercise08_09.arrayExercise08()
+//arrayExercise08_09.arrayExercise09()
