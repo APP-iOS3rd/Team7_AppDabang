5436414504be6d076896afc620a54942aeafa576
Phangg <leeckdwns9305@gmail.com>
2023-10-29 23:34:31 +0900

    :seedling: Mentoring Prac
    app start view (app name), loginview

---
+++ lecture/changjun/mentoring 1026-1031/MentoringPrac/MentoringPrac/MentoringPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentoringPracApp.swift
+//  MentoringPrac
+//
+//  Created by phang on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MentoringPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
