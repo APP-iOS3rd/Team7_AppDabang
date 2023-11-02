9832cd80ad474e27a9c89e976f91885ecf675704
SeonKoo <sungu09@naver.com>
2023-11-02 16:53:13 +0900

    Movie pix

---
+++ MovieDabang/MovieDabang/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  MovieDabang
+//
+//  Created by 박선구 on 11/2/23.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
