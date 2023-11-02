bebe0ca5b126617c82e9016acf7266e7d67df929
SeonKoo <sungu09@naver.com>
2023-11-02 16:55:26 +0900

    Movie fix2

--- MovieDabang/MovieDabang/MovieDetailView.swift
+++ MovieDabang/MovieDabang/MovieDetailView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct MovieDetailView: View {
+    
+    struct Movie: Codable, Identifiable {
+        let id: Int
+        let title: String
+        let overview: String
+        let posterPath: String
+        let releaseDate: String
+        let voteAverage: Double
+    }
+    
+    @State var like : Bool = false
+    
+//    let Movie = ["name" : "과속스캔들", "date" : "2007.11.14", "people" : "264K", "imageName" : "testImage"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(content: {
+                Image("testImage")
+                    .resizable()
+                    .cornerRadius(12.0)
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                Text("과속스캔들")
+                    .font(.largeTitle)
+                
+                Text("2007.11.14")
+                    .font(.headline)
+                
+                Text("264만명")
+                    .font(.headline)
+                Spacer()
+                HStack{
+                    Text("좋아요")
+                    Spacer()
+                    Image(systemName: like == false ? "heart" : "heart.fill")
+                        .resizable()
+                        .frame(width: 30,height: 30)
+                        .onTapGesture {
+                            touchHeart()
+                        }
+                }
+                .padding()
+            })
+        }
+    }
+    func touchHeart() {
+        if like == false{
+            like = true
+        }else{
+            like = false
+        }
     }
 }
-
 #Preview {
     MovieDetailView()
 }
