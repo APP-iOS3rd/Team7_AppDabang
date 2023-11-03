437f64e721baf01c25f22650b9b144b37e264114
SeonKoo <sungu09@naver.com>
2023-11-03 17:49:31 +0900

    nothing change

--- MovieDabang/MovieDabang/ContentView.swift
+++ MovieDabang/MovieDabang/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "mug")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
